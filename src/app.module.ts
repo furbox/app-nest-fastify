52cf82aab59c7b80724e0c5182a65513f05ea3f0
furbox <furbox@gmail.com>
2021-09-12 19:04:52 -0500

    optimize app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
@@ -14,8 +12,6 @@ import { MongooseModule } from '@nestjs/mongoose';
       useFindAndModify: false,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {
   static port: number | string;
